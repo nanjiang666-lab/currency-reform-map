5bd1ce9a6994812bb483609e2f6f941938895f6c
nanjiang666-lab <njian29@emory.edu>
2025-05-17 01:03:30 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,10 @@ export default function Home() {
       zoom:1.5
     });
     map.on('load', ()=>{
-      map.addSource('countries',{type:'geojson',data:countriesData});
+      map.addSource('countries',{
+type: 'geojson',
+data: '/countries.geojson'    // ← 从 public 目录下加载
+});
       map.addLayer({ id:'fill', type:'fill', source:'countries', paint:{'fill-color':'#627BC1','fill-opacity':0.5} });
       map.addLayer({ id:'border', type:'line', source:'countries', paint:{'line-color':'#fff','line-width':0.5} });
     });
