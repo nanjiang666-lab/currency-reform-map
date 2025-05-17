3c460c618027580f714e146950137b83400603f5
nanjiang666-lab <njian29@emory.edu>
2025-05-17 00:42:56 -0400

    Create _app.js

---
+++ src/pages/_app.js
@@ -0,0 +1,11 @@
+import '../styles/globals.css';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { SessionProvider } from 'next-auth/react';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+}
